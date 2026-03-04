ef754105e914a6c75906c4bfedf334b18e4c401d
Ransilu456 <samarasekarakeshan@gmail.com>
2026-03-04 23:32:56 +0530

    BLog Updated

--- components/data/blog.ts
+++ components/data/blog.ts
@@ -1,36 +1,18 @@
 import { BlogPost } from "./types";
 
-import { death_of_keywords_topic_clusters } from "./articles/death_of_keywords_topic_clusters";
 import { nextjs_vs_wordpress_for_seo } from "./articles/nextjs_vs_wordpress_for_seo";
-import { ssr_vs_ssg_isr } from "./articles/ssr_vs_ssg_isr";
-import { optimize_core_web_vitals_ecommerce } from "./articles/optimize_core_web_vitals_ecommerce";
-import { schema_markup_rich_snippets } from "./articles/schema_markup_rich_snippets";
 import { high_converting_facebook_posters } from "./articles/high_converting_facebook_posters";
-import { social_media_marketing_mistakes } from "./articles/social_media_marketing_mistakes";
-import { mastering_google_ads_2026 } from "./articles/mastering_google_ads_2026";
-import { whatsapp_marketing_for_small_business } from "./articles/whatsapp_marketing_for_small_business";
 import { building_brand_communities } from "./articles/building_brand_communities";
 import { future_of_ai_branding } from "./articles/future_of_ai_branding";
-import { building_scalable_design_systems } from "./articles/building_scalable_design_systems";
 import { dark_mode_design_psychology } from "./articles/dark_mode_design_psychology";
-import { micro_interactions_conversion } from "./articles/micro_interactions_conversion";
 import { accessible_design_is_better_design } from "./articles/accessible_design_is_better_design";
 
 export const blogPosts: BlogPost[] = [
-  death_of_keywords_topic_clusters,
   nextjs_vs_wordpress_for_seo,
-  ssr_vs_ssg_isr,
-  optimize_core_web_vitals_ecommerce,
-  schema_markup_rich_snippets,
   high_converting_facebook_posters,
-  social_media_marketing_mistakes,
-  mastering_google_ads_2026,
-  whatsapp_marketing_for_small_business,
   building_brand_communities,
   future_of_ai_branding,
-  building_scalable_design_systems,
   dark_mode_design_psychology,
-  micro_interactions_conversion,
   accessible_design_is_better_design
 ];
 
